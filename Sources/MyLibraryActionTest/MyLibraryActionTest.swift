c9a150fec5dac6d52175979457764dd4bc1ab9a1
Andrea <andrea.alessandro@gmail.com>
2021-03-12 09:29:52 +0100

    added jazzy

--- Sources/MyLibraryActionTest/MyLibraryActionTest.swift
+++ Sources/MyLibraryActionTest/MyLibraryActionTest.swift
@@ -1,3 +1,9 @@
+/// Testing documentation
 struct MyLibraryActionTest {
     var text = "Hello, World!"
+    
+    /// Printing text function
+    func printText() {
+        print("\(text)")
+    }
 }
